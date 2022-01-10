796bd5ac04545d198a80c32609737b1431f1e8ee
YuanJianzheng <yuanguangyu1221@163.com>
2022-01-10 16:51:15 +0800

    refactor for use any type constraint

--- filter_by_generic.go
+++ filter_by_generic.go
@@ -1,11 +1,12 @@
 package main
 
-type FilterElem interface {
-	int | float64
-}
+// type FilterElem interface {
+// 	int | float64
+// }
 
-// type parameter also can like: FilterByGeneric[V int | float64]
-func FilterByGeneric[V FilterElem](elems []V, predicate func(V)bool) []V {
+// type parameter also can like: func FilterByGeneric[V int | float64]
+// type parameter also can like: func FilterByGeneric[V FilterElem]
+func FilterByGeneric[V any](elems []V, predicate func(V)bool) []V {
 	var r []V
 	for _, e := range elems {
 		if predicate(e) {
